57282b278f0aaa92a2106ebc2bd8efea7798c794
月色真美 <89786902+LauZzL@users.noreply.github.com>
2025-08-02 14:18:00 +0800

    Update pyxyz-monkey.user.js
    fix down not defined

--- pyxyz-monkey.user.js
+++ pyxyz-monkey.user.js
@@ -10,7 +10,7 @@
 // @grant      GM_addStyle
 // ==/UserScript==
 
-(t=>{if(typeof GM_addStyle=="function"){GM_addStyle(t);return}const e=document.createElement("style");e.textContent=t,document.head.append(e)})(' .pyxyz-container[data-v-63531431]{display:flex;justify-content:center;align-items:center;height:100vh;width:100vw;overflow:hidden}.pyxyz-card-container[data-v-63531431]{width:433px;height:333px}@media (max-width: 767px){.pyxyz-card-container[data-v-63531431]{width:100%;height:777px;font-size:33px}}.file-container[data-v-63531431]{display:flex;justify-content:center;align-items:center}.file-info[data-v-63531431]{display:flex;flex-direction:column;align-items:flex-start}html,body{width:100%;height:100%}input::-ms-clear,input::-ms-reveal{display:none}*,*:before,*:after{box-sizing:border-box}html{font-family:sans-serif;line-height:1.15;-webkit-text-size-adjust:100%;-ms-text-size-adjust:100%;-ms-overflow-style:scrollbar;-webkit-tap-highlight-color:rgba(0,0,0,0)}@-ms-viewport{width:device-width}body{margin:0}[tabindex="-1"]:focus{outline:none}hr{box-sizing:content-box;height:0;overflow:visible}h1,h2,h3,h4,h5,h6{margin-top:0;margin-bottom:.5em;font-weight:500}p{margin-top:0;margin-bottom:1em}abbr[title],abbr[data-original-title]{-webkit-text-decoration:underline dotted;text-decoration:underline;text-decoration:underline dotted;border-bottom:0;cursor:help}address{margin-bottom:1em;font-style:normal;line-height:inherit}input[type=text],input[type=password],input[type=number],textarea{-webkit-appearance:none}ol,ul,dl{margin-top:0;margin-bottom:1em}ol ol,ul ul,ol ul,ul ol{margin-bottom:0}dt{font-weight:500}dd{margin-bottom:.5em;margin-left:0}blockquote{margin:0 0 1em}dfn{font-style:italic}b,strong{font-weight:bolder}small{font-size:80%}sub,sup{position:relative;font-size:75%;line-height:0;vertical-align:baseline}sub{bottom:-.25em}sup{top:-.5em}pre,code,kbd,samp{font-size:1em;font-family:SFMono-Regular,Consolas,Liberation Mono,Menlo,Courier,monospace}pre{margin-top:0;margin-bottom:1em;overflow:auto}figure{margin:0 0 1em}img{vertical-align:middle;border-style:none}a,area,button,[role=button],input:not([type=range]),label,select,summary,textarea{touch-action:manipulation}table{border-collapse:collapse}caption{padding-top:.75em;padding-bottom:.3em;text-align:left;caption-side:bottom}input,button,select,optgroup,textarea{margin:0;color:inherit;font-size:inherit;font-family:inherit;line-height:inherit}button,input{overflow:visible}button,select{text-transform:none}button,html [type=button],[type=reset],[type=submit]{-webkit-appearance:button}button::-moz-focus-inner,[type=button]::-moz-focus-inner,[type=reset]::-moz-focus-inner,[type=submit]::-moz-focus-inner{padding:0;border-style:none}input[type=radio],input[type=checkbox]{box-sizing:border-box;padding:0}input[type=date],input[type=time],input[type=datetime-local],input[type=month]{-webkit-appearance:listbox}textarea{overflow:auto;resize:vertical}fieldset{min-width:0;margin:0;padding:0;border:0}legend{display:block;width:100%;max-width:100%;margin-bottom:.5em;padding:0;color:inherit;font-size:1.5em;line-height:inherit;white-space:normal}progress{vertical-align:baseline}[type=number]::-webkit-inner-spin-button,[type=number]::-webkit-outer-spin-button{height:auto}[type=search]{outline-offset:-2px;-webkit-appearance:none}[type=search]::-webkit-search-cancel-button,[type=search]::-webkit-search-decoration{-webkit-appearance:none}::-webkit-file-upload-button{font:inherit;-webkit-appearance:button}output{display:inline-block}summary{display:list-item}template{display:none}[hidden]{display:none!important}mark{padding:.2em;background-color:#feffe6} ');
+(t=>{if(typeof GM_addStyle=="function"){GM_addStyle(t);return}const e=document.createElement("style");e.textContent=t,document.head.append(e)})(' .pyxyz-container[data-v-4e5f96a4]{display:flex;justify-content:center;align-items:center;height:100vh;width:100vw;overflow:hidden}.pyxyz-card-container[data-v-4e5f96a4]{width:433px;height:333px}@media (max-width: 767px){.pyxyz-card-container[data-v-4e5f96a4]{width:100%;height:777px;font-size:33px}}.file-container[data-v-4e5f96a4]{display:flex;justify-content:center;align-items:center}.file-info[data-v-4e5f96a4]{display:flex;flex-direction:column;align-items:flex-start}html,body{width:100%;height:100%}input::-ms-clear,input::-ms-reveal{display:none}*,*:before,*:after{box-sizing:border-box}html{font-family:sans-serif;line-height:1.15;-webkit-text-size-adjust:100%;-ms-text-size-adjust:100%;-ms-overflow-style:scrollbar;-webkit-tap-highlight-color:rgba(0,0,0,0)}@-ms-viewport{width:device-width}body{margin:0}[tabindex="-1"]:focus{outline:none}hr{box-sizing:content-box;height:0;overflow:visible}h1,h2,h3,h4,h5,h6{margin-top:0;margin-bottom:.5em;font-weight:500}p{margin-top:0;margin-bottom:1em}abbr[title],abbr[data-original-title]{-webkit-text-decoration:underline dotted;text-decoration:underline;text-decoration:underline dotted;border-bottom:0;cursor:help}address{margin-bottom:1em;font-style:normal;line-height:inherit}input[type=text],input[type=password],input[type=number],textarea{-webkit-appearance:none}ol,ul,dl{margin-top:0;margin-bottom:1em}ol ol,ul ul,ol ul,ul ol{margin-bottom:0}dt{font-weight:500}dd{margin-bottom:.5em;margin-left:0}blockquote{margin:0 0 1em}dfn{font-style:italic}b,strong{font-weight:bolder}small{font-size:80%}sub,sup{position:relative;font-size:75%;line-height:0;vertical-align:baseline}sub{bottom:-.25em}sup{top:-.5em}pre,code,kbd,samp{font-size:1em;font-family:SFMono-Regular,Consolas,Liberation Mono,Menlo,Courier,monospace}pre{margin-top:0;margin-bottom:1em;overflow:auto}figure{margin:0 0 1em}img{vertical-align:middle;border-style:none}a,area,button,[role=button],input:not([type=range]),label,select,summary,textarea{touch-action:manipulation}table{border-collapse:collapse}caption{padding-top:.75em;padding-bottom:.3em;text-align:left;caption-side:bottom}input,button,select,optgroup,textarea{margin:0;color:inherit;font-size:inherit;font-family:inherit;line-height:inherit}button,input{overflow:visible}button,select{text-transform:none}button,html [type=button],[type=reset],[type=submit]{-webkit-appearance:button}button::-moz-focus-inner,[type=button]::-moz-focus-inner,[type=reset]::-moz-focus-inner,[type=submit]::-moz-focus-inner{padding:0;border-style:none}input[type=radio],input[type=checkbox]{box-sizing:border-box;padding:0}input[type=date],input[type=time],input[type=datetime-local],input[type=month]{-webkit-appearance:listbox}textarea{overflow:auto;resize:vertical}fieldset{min-width:0;margin:0;padding:0;border:0}legend{display:block;width:100%;max-width:100%;margin-bottom:.5em;padding:0;color:inherit;font-size:1.5em;line-height:inherit;white-space:normal}progress{vertical-align:baseline}[type=number]::-webkit-inner-spin-button,[type=number]::-webkit-outer-spin-button{height:auto}[type=search]{outline-offset:-2px;-webkit-appearance:none}[type=search]::-webkit-search-cancel-button,[type=search]::-webkit-search-decoration{-webkit-appearance:none}::-webkit-file-upload-button{font:inherit;-webkit-appearance:button}output{display:inline-block}summary{display:list-item}template{display:none}[hidden]{display:none!important}mark{padding:.2em;background-color:#feffe6} ');
 
 (function (vue) {
   'use strict';
@@ -20,7 +20,7 @@
     return mod || (0, cb[__getOwnPropNames(cb)[0]])((mod = { exports: {} }).exports, mod), mod.exports;
   };
   var require_main_001 = __commonJS({
-    "main-DFlWkGL3.js"(exports, module) {
+    "main-5gV51P98.js"(exports, module) {
       function bind(fn, thisArg) {
         return function wrap() {
           return fn.apply(thisArg, arguments);
@@ -95486,18 +95486,18 @@ summary tabindex target title type usemap value width wmode wrap`;
           const toRefererDownload = () => {
             const u2 = new URL(domain);
             let referer = u2.searchParams.get("referer");
-            let down2 = u2.searchParams.get("down");
+            let down = u2.searchParams.get("down");
             let name2 = u2.searchParams.get("name");
-            if (referer && down2 && name2) {
+            if (referer && down && name2) {
               referer = gBase64.decode(referer);
-              down2 = gBase64.decode(down2);
+              down = gBase64.decode(down);
               name2 = gBase64.decode(name2);
               Modal.info({
                 title: "提示",
                 content: `开始下载：${name2}`,
                 cancelText: "取消",
                 onOk: () => {
-                  window.open(down2);
+                  window.open(down);
                 },
                 onCancel: () => {
                   api$1.success("取消下载");
@@ -95523,18 +95523,18 @@ summary tabindex target title type usemap value width wmode wrap`;
           const getFileInfo = () => {
             const u2 = new URL(location.href);
             let referer = u2.searchParams.get("referer");
-            let down2 = u2.searchParams.get("down");
+            let down = u2.searchParams.get("down");
             let name2 = u2.searchParams.get("name");
-            if (!referer || !down2 || !name2) {
+            if (!referer || !down || !name2) {
               alert("参数错误");
               return;
             }
             referer = gBase64.decode(referer);
-            down2 = gBase64.decode(down2);
+            down = gBase64.decode(down);
             name2 = gBase64.decode(name2);
             return {
               referer,
-              down: down2,
+              down,
               name: name2
             };
           };
@@ -95560,13 +95560,12 @@ summary tabindex target title type usemap value width wmode wrap`;
                   content: `开始下载：${name}`,
                   cancelText: "取消",
                   onOk: () => {
-                    window.open(down);
+                    window.open(fileInfo.value.down);
                   },
                   onCancel: () => {
                     api$1.success("取消下载");
                   }
                 });
-                window.open(fileInfo.value.down);
                 return;
               }
               const newUrl = new URL(fileInfo.value.referer);
@@ -95802,7 +95801,7 @@ summary tabindex target title type usemap value width wmode wrap`;
           };
         }
       };
-      const App = /* @__PURE__ */ _export_sfc(_sfc_main, [["__scopeId", "data-v-63531431"]]);
+      const App = /* @__PURE__ */ _export_sfc(_sfc_main, [["__scopeId", "data-v-4e5f96a4"]]);
       vue.createApp(App).use(Antd).mount(
         (() => {
           const app = document.createElement("div");
